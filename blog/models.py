56586fb8b0eb49faef69a8ecb588936eeca08b36
AMartins87 <aneta.martins@outlook.com>
2022-06-23 15:54:08 +0000

    Created model Post and fixed urls.py - missing code

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    title_tag = models.CharField(max_length=250, default="blog")
+    body = models.TextField(blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name="blog_post")
+
+    def __str__(self):
+        return self.title
