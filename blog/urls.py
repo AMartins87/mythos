cdff6af8250bb2ccdac7399ebe886ae8416379f8
AMartins87 <aneta.martins@outlook.com>
2022-06-25 12:00:42 +0000

    Add addpost and stories html templates. Install ckeditor.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import HomeView
+from django.urls import path, include
+from .views import HomeView, StoryView, AddPostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
+    path('blog/<int:pk>', StoryView.as_view(), name="blog"),
+    path('new-post/', AddPostView.as_view(), name="new-post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 from .forms import PostForm
 
@@ -9,3 +9,14 @@ from .forms import PostForm
 class HomeView(ListView):
     model = Post
     template_name = 'index.html'
+
+
+class StoryView(DetailView):
+    model = Post
+    template_name = 'stories.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'addpost.html'
