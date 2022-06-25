de460c3787ff1b32a05504cf02cac47d11587911
AMartins87 <aneta.martins@outlook.com>
2022-06-25 18:24:32 +0000

    Add like field and a function to count likes. Created Comment model

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,23 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=250, default="blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE, default="")
     body = RichTextField(blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name="blog_post")
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
         return reverse('home')
+
+    def total_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
