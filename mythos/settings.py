d39fbb24bd7070787323f7c9444d788fdd5d51a7
AMartins87 <aneta.martins@outlook.com>
2022-07-08 20:35:18 +0000

    Deleted alt as not allowed within div element

--- mythos/settings.py
+++ mythos/settings.py
@@ -97,20 +97,6 @@ else:
         }
     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
